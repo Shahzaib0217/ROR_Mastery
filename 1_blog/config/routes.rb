d9c16a2cf7c463e7c71d75988d033f4093d29d13
Shahzaib0217 <shahzaib.khan0217@gmail.com>
2024-12-13 15:21:17 +0500

    Index Route

--- 1_blog/config/routes.rb
+++ 1_blog/config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show]
+
+  # adding only, creates the mentioned routes only
+  resources :articles, only: [:show, :index]
 end
